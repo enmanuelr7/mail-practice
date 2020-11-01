3f194ab837a20e671a970f76b9256b9031f1a452
Enmanuel Dario Ramirez Villamizar <e.ramirez.villamizar@accenture.com>
2020-10-31 21:58:24 -0300

    add name property to transporter

--- src/index.js
+++ src/index.js
@@ -6,28 +6,27 @@ const app = express()
 app.use(express.json())
 
 app.post('/', async (req, res) => {
-    let transporter = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
+        name: 'dekorachile',
         host: "mail.dekorachile.com",
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: 'noreply@dekorachile.com', // generated ethereal user
-            pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+            user: 'noreply@dekorachile.com',
+            pass: process.env.EMAIL_PASSWORD,
         },
         tls: {
             rejectUnauthorized: false
         }
     });
 
-    // send mail with defined transport object
-
     try {
         let info = await transporter.sendMail({
-            from: 'noreply@dekorachile.com', // sender address
-            to: "enmanuel_ram@hotmail.com", // list of receivers
-            subject: "Gracias por tu compra", // Subject line
-            text: `hola ${req.body.name}, gracias por tu compra`, // plain text body
-            // html: `hola ${req.body.name}, gracias por tu compra`, // html body
+            from: '"Dekora Chile" <noreply@dekorachile.com>', // sender address
+            to: "enmanuelr7@gmail.com", // list of receivers
+            subject: "prueba 3",
+            text: `hola ${req.body.name}, gracias por tu compra`,
+            html: `<h1>hola ${req.body.name}, gracias por tu compra</h1>`
         });
         res.send("Message sent: %s " + info.messageId);
     } catch (error) {
